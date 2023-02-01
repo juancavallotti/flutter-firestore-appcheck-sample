c89093c6932a16e13fd8e9f071a941fb22cf9507
Juan Alberto López Cavallotti <juancavallotti@gmail.com>
2023-02-01 14:43:18 -0300

    Added firebase and .env to support firebase secrets

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:reminders_app/app/home_screen.dart';
 import 'package:reminders_app/app/home_state.dart';
+import 'package:reminders_app/firebase_options.dart';
 
-void main() {
+void main() async {
+  await dotenv.load();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
