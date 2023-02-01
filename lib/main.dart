45fd78257045d15cae6cc3a6b78c12bfb42d0546
Juan Alberto López Cavallotti <juancavallotti@gmail.com>
2023-02-01 12:56:41 -0300

    Added bloc provider to the main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:reminders_app/app/home_screen.dart';
+import 'package:reminders_app/app/home_state.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,8 +12,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeScreen(),
-    );
+    return MaterialApp(
+        home: BlocProvider(
+      create: (context) => RemindersAppStateCubit(),
+      child: const HomeScreen(),
+    ));
   }
 }
