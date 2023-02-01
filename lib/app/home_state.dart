d78a40aa06004dcf015d8653d1fd7eec48a34828
Juan Alberto López Cavallotti <juancavallotti@gmail.com>
2023-02-01 12:53:19 -0300

    Created initial state management infrastructure

---
+++ lib/app/home_state.dart
@@ -0,0 +1,26 @@
+/*
+Classes related to the home screens's state management.
+*/
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'reminder.dart';
+
+abstract class RemindersAppState {}
+
+class LoadingReminders extends RemindersAppState {}
+
+class RemindersAvailable extends RemindersAppState {
+  final List<Reminder> reminders;
+
+  RemindersAvailable(this.reminders);
+}
+
+class Uninitialized extends RemindersAppState {}
+
+/*
+And the cubit.
+*/
+
+class RemindersAppStateCubit extends Cubit<RemindersAppState> {
+  RemindersAppStateCubit() : super(Uninitialized());
+}
